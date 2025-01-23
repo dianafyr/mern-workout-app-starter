1ab3bb86191519f21db3f35bd3339512268724bf
fyrdiana <23016424@myrp.edu.sg>
2025-01-23 09:21:51 +0800

    lol

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
         <header>
             <div classNAme="container">
                 <Link to="/">
-                    <h1>Workout Body</h1>
+                    <h1>Workout Buddy</h1>
                 </Link>
             </div>
         </header>
